d533e59fc227ced84af4a24677b7edc4e8e6ea6a
adust09 <shoukitsuda@icloud.com>
2025-02-18 07:55:34 +0900

    add local faucet

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,30 @@
+import { task } from "hardhat/config";
 import type { HardhatUserConfig } from "hardhat/config";
+
+import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
+
+task("sendEth", "Send eth to the specified address").setAction(
+  async (_, { ethers }) => {
+    const recipientAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+    const amount = ethers.parseEther("100");
+    const sender = await ethers.provider.getSigner();
+    const tx = await sender.sendTransaction({
+      to: recipientAddress,
+      value: amount,
+    });
+
+    console.log("Transaction hash:", tx.hash);
+  }
+);
 
 export default config;
