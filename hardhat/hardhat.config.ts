f298dffa8f74c75bb7867ae0a66bce5df5886dc1
adust09 <shoukitsuda@icloud.com>
2025-02-17 09:27:00 +0900

    delete unused packages

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,6 +1,5 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import "tsconfig-paths/register";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
